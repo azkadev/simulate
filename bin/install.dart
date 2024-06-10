41794830cabbf26e62d0f55bf46c4f7aaac47f01
￴￴ <>
2024-06-10 11:34:58 +0700

    update

---
+++ bin/install.dart
@@ -0,0 +1,6 @@
+import 'package:general_lib/general_lib.dart';
+import 'package:universal_io/io.dart';
+
+void main(List<String> args) {
+  Dart.pub.installFromDirectory(directoryPackage: Directory.current).printPretty();
+}
